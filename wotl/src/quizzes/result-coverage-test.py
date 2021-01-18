adece3e4da2dde6919313c4e81bb707d87e47983
Joris Barkema <jorisbarkema@gmail.com>
2021-01-18 15:07:07 +0100

    Add questions for the talent quiz

--- wotl/src/quizzes/result-coverage-test.py
+++ wotl/src/quizzes/result-coverage-test.py
@@ -28,14 +28,15 @@ def take_quiz(quiz):
         for p in points[answer]:
             result.setdefault(p, 0)
             result[p] += points[answer][p]
-        
+    
+    #print(result)
     return best(result)
 
 def coverage(quiz):
     s = 0
     results = {}
 
-    for _ in range(1000):
+    for _ in range(5000):
         r = take_quiz(quiz)
 
         results.setdefault(r, 0)
@@ -56,16 +57,17 @@ if __name__ == "__main__":
             quiz = json.load(f)
             results = coverage(quiz)
 
-            print('coverage for ' + quiz_name)
-            
+            length_name = len('coverage for ' + quiz_name)
+            longest = length_name
 
-            l = 0
             for r in results:
-                if len(r) > l: l = len(r)
+                if len(r) > longest: longest = len(r)
             
-            print((l + 6) * '-')
-            for r in results:
-                print(str(r) + ':' + ((1 + l - len(r)) * ' ') + str(round(results[r], 2)))
-            print((l + 6) * '-')
+            print('+-' + (longest + 6) * '-' + '-+')
+            print('| coverage for ' + quiz_name + ((6 + longest - length_name) * ' ') + ' |')
+            print('+-' + (longest + 6) * '-' + '-+')
+            for r in dict(sorted(results.items(), key=lambda item: item[1])):
+                print('| ' + str(r) + ':' + ((1 + longest - len(r)) * ' ') + "{:.2f}".format(results[r]) + ' |')
+            print('+-' + (longest + 6) * '-' + '-+')
     else:
         print("Unexpected inputs")
